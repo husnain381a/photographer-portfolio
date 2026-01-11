7166580b14236886ed8989ca8111ab3bfde8ec4b
Hunsain Mazhar <22101001-008@uskt.edu.pk>
2026-01-11 22:32:00 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ServicePlans from './components/ServicePlans'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <ServicePlans />
+    </div>
+  )
+}
+
+export default App
